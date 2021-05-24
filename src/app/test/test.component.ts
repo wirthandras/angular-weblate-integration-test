774a155d1aae685da06086f91e2da0dad95cd252
Andras Wirth <agrippa.wia@gmail.com>
2021-05-24 20:01:49 +0200

    update localization build

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
 
